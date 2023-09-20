b04df048383d351ebb128459d156cac028ded67d
Prapanchj <145548350+Prapanchj@users.noreply.github.com>
2023-09-20 14:31:21 +0530

    Add files via upload

---
+++ Sum.java
@@ -0,0 +1,14 @@
+/********************************************************************************
+ * Name				:Hellojava.java
+ * Author			:Prapanch J
+ * Description		:Java Program that adds two numbers.
+ * Version			:1.0
+ * Date				:20/09/23
+ * ******************************************************************************/
+public class Sum {
+	public static void main (String [] args) {
+		int num1 = 20, num2 = 30;
+		int sum=num1+num2;
+		System.out.println("sum="+ sum);
+	}
+}
