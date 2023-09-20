59ec62da933b201e0fee8f54150b903d45288985
Prapanchj <145548350+Prapanchj@users.noreply.github.com>
2023-09-20 14:27:23 +0530

    Add files via upload

---
+++ Hellojava.java
@@ -0,0 +1,12 @@
+/********************************************************************************
+ * Name				:Hellojava.java
+ * Author			:Prapanch J
+ * Description		:My First Java Program that prints "Hello Java"
+ * Version			:1.0
+ * Date				:20/09/23
+ * ******************************************************************************/
+public class Hellojava {
+	public static void main (String [] args) {
+		System.out.println("Hello Java");
+	}
+}
