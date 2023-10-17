87adba426a39442e270942838e1a30c2a0548442
Prapanchj <145548350+Prapanchj@users.noreply.github.com>
2023-10-17 16:56:23 +0530

    Add files via upload

---
+++ frequency.java
@@ -0,0 +1,35 @@
+package palindrome;
+import java.util.Scanner;
+
+public class frequency {
+	public static void main(String[]args) {
+		String line;
+		Scanner sc= new Scanner(System.in);
+		System.out.println("Enter the string");
+		line=sc.nextLine();
+		System.out.println("Enter the character to be Searched");
+		char character=sc.next().charAt(0);
+		int Frequency=Freq(line,character);
+		if(Frequency==0){
+			System.out.println("The Character is not present in the String");
+		}
+		else {
+		System.out.println("Frequency is "+Frequency);
+		}
+	}
+		static  int Freq(String line,char character) {
+			char[] CharArray= line.toCharArray();
+			int Strlength=line.length(),count=0;
+		    for(int i=0;i<Strlength;i++){
+		    	if(CharArray[i]==character)
+		    	{
+		    		count++;		    				
+		    	}
+		    }
+		    return count;
+		}
+}
+
+		    
+
+
---
+++ pldrm.java
@@ -0,0 +1,38 @@
+
+package palindrome;
+import java.util.Scanner;
+public class pldrm {
+	public static void main (String[] args) {
+	
+		Scanner sc= new Scanner(System.in);
+		String Character;
+		System.out.println("Enter the string");
+		Character=sc.next();
+		boolean isPalindrome=Check(Character);
+		if(isPalindrome==false) {
+		 System.out.println("It is a palindrome");
+		}
+		else {
+			System.out.println("It is not a palindrome");
+		}
+	}
+		static  boolean Check(String Character) {
+			char[] CharArray= Character.toCharArray();
+			int Strlength=Character.length();
+		    for(int i=0;i<Strlength/2;i++){
+		    	if(CharArray[i]==CharArray[Strlength-i-1])
+		    	{
+		    		return false;
+		    				
+		    	}
+		    return true;
+		    	
+		    }
+		    return false;
+		}
+}
+		    
+		
+			
+	
+
