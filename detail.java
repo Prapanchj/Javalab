f61c5741a1217768ec4bd56c2907209880daa37a
Prapanchj <145548350+Prapanchj@users.noreply.github.com>
2023-11-17 15:44:01 +0530

    Update detail.java

--- detail.java
+++ detail.java
@@ -1,3 +1,11 @@
+/*******************************************************************************
+ * Name				:Hellojava.java
+ * Author			:Prapanch J
+ * Description		:My First Java Program that prints "Hello Java"
+ * Version			:1.0
+ * Date				:20/09/23
+ * ******************************************************************************/
+
 package employe;
 import java.util.Scanner;
 public class detail {
